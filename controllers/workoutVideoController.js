5fe70c0c238c2692547b311f605e1db38f702a4b
shine2k <2kshinemhrzn@gmail.com>
2022-11-03 16:48:28 +1100

    Created and Testing all CRUD operations

--- controllers/workoutVideoController.js
+++ controllers/workoutVideoController.js
@@ -14,14 +14,80 @@ exports.uploadWorkoutVideos = async (req, res) => {
     const workoutVideo = await WorkoutVideo.create(req.body);
     res.status(200).json({
       status: "success",
-      data: {WorkoutVideo},
+      data: { WorkoutVideo },
     });
   } catch (e) {
-    console.log(e)
+    console.log(e);
     res.status(400).json({
       status: "failed",
     });
   }
 };
 
+exports.getAllWorkoutVideos = async (req, res) => {
+  try {
+    const workoutVideos = await WorkoutVideo.find();
+    res.status(200).json({
+      status: "success",
+      data: {
+        workoutVideos,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "failed",
+    });
+  }
+};
 
+exports.deleteWorkoutVideos = async (req, res) => {
+  try {
+    const workoutVideos = await WorkoutVideo.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "failed",
+    });
+  }
+};
+
+exports.updateWorkoutVideos = async (req, res) => {
+  try {
+    const workoutVideos = await WorkoutVideo.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json({
+      status: "success",
+      data: {
+        workoutVideos,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "failed",
+    });
+  }
+};
+
+exports.getSingleWorkoutVideo = async (req, res) => {
+  try {
+    const workoutVideo = await WorkoutVideo.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        workoutVideo,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "failed",
+    });
+  }
+};
--- routes/workoutVideoRoutes.js
+++ routes/workoutVideoRoutes.js
@@ -5,6 +5,15 @@ const workoutVideoController = require("../controllers/workoutVideoController");
 //Creating express route
 const router = express.Router();
 
-router.route("/").post(workoutVideoController.uploadWorkoutVideos);
+router
+  .route("/")
+  .post(workoutVideoController.uploadWorkoutVideos)
+  .get(workoutVideoController.getAllWorkoutVideos);
+
+router
+  .route("/:id")
+  .get(workoutVideoController.getSingleWorkoutVideo)
+  .delete(workoutVideoController.deleteWorkoutVideos)
+  .patch(workoutVideoController.updateWorkoutVideos);
 
 module.exports = router;
